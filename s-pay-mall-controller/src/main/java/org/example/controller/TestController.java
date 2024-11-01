e129aaf178e8b0934367a8a62e285c19f6903fc1
zqiusu <1332763529@qq.com>
2024-11-01 17:19:30 +0800

    day03:登录界面开发

--- s-pay-mall-controller/src/main/java/org/example/controller/TestController.java
+++ s-pay-mall-controller/src/main/java/org/example/controller/TestController.java
@@ -2,20 +2,18 @@ package org.example.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.domain.res.WeixinQrCodeRes;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 
 //localhost:80/Test/getRes
 
 @Slf4j
-@RestController()
 @RequestMapping("Test")
 public class TestController {
+    @ResponseBody
     @GetMapping("/getRes")
-    public WeixinQrCodeRes get(@RequestParam(value="name") String name){
+    public WeixinQrCodeRes get(@RequestParam(value="name",required = false) String name){
         WeixinQrCodeRes weixinQrCodeRes = new WeixinQrCodeRes();
         weixinQrCodeRes.setUrl("123");
         System.out.println(name);
--- s-pay-mall-service/src/main/java/org/example/service/weixin/IWeixinApiService.java
+++ s-pay-mall-service/src/main/java/org/example/service/weixin/IWeixinApiService.java
@@ -6,11 +6,13 @@ import org.example.domain.res.WeixinQrCodeRes;
 import org.example.domain.res.WeixinTokenRes;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface IWeixinApiService {
 
+    @GET("cgi-bin/token")
     Call<WeixinTokenRes> getToken(@Query("grant_type") String grantType,
                                   @Query("appid") String appId,
                                   @Query("secret") String appSecret);
