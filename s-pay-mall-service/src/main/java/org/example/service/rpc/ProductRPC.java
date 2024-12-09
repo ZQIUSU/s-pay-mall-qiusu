3e4486cab069cfe4bc656b8e12cc6d625a006ceb
zqiusu <1332763529@qq.com>
2024-12-09 11:32:28 +0800

    day08:部署

--- s-pay-mall-service/src/main/java/org/example/service/rpc/ProductRPC.java
+++ s-pay-mall-service/src/main/java/org/example/service/rpc/ProductRPC.java
@@ -13,7 +13,7 @@ public class ProductRPC {
         productVO.setProductId(productId);
         productVO.setProductName("测试商品");
         productVO.setProductDesc("这是一个测试商品");
-        productVO.setPrice(new BigDecimal("1.68"));
+        productVO.setPrice(new BigDecimal("238.8"));
         return productVO;
     }
 
