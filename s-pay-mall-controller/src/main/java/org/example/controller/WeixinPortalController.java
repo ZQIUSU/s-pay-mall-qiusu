46b76e6f042c1d186f150cff1d31a2748a7d7412
zqiusu <1332763529@qq.com>
2025-02-25 08:52:59 +0800

    Revert "feat:添加备案号，链接至"http://beian.miit.gov.cn/""
    This reverts commit 982b26d8530e586c99712ee75abf33592be9d702.

--- s-pay-mall-controller/src/main/java/org/example/controller/WeixinPortalController.java
+++ s-pay-mall-controller/src/main/java/org/example/controller/WeixinPortalController.java
@@ -3,17 +3,16 @@ package org.example.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.example.common.weixin.MessageTextEntity;
-import org.example.common.weixin.SignatureUtil;
-import org.example.common.weixin.XmlUtil;
-import org.example.service.ILoginService;
+import org.example.common.MessageTextEntity;
+import org.example.common.SignatureUtil;
+import org.example.common.XmlUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.Resource;
-
 /**
- * https://apis.zqiusu.site/api/v1/weixin/portal/receive/
+ * 微信服务对接，对接地址：<a href="http://xfg-studio.natapp1.cc/api/v1/weixin/portal/receive">/api/v1/weixin/portal/receive</a>
+ * <p>
+ * http://zqiusu-studio.natapp1.cc/api/v1/weixin/portal/receive/
  */
 @Slf4j
 @RestController()
@@ -25,9 +24,6 @@ public class WeixinPortalController {
     private String originalid;
     @Value("${weixin.config.token}")
     private String token;
-
-    @Resource
-    private ILoginService loginService;
 
     @GetMapping(value = "receive", produces = "text/plain;charset=utf-8")
     public String validate(@RequestParam(value = "signature", required = false) String signature,
@@ -61,14 +57,8 @@ public class WeixinPortalController {
                        @RequestParam(name = "msg_signature", required = false) String msgSignature) {
         try {
             log.info("接收微信公众号信息请求{}开始 {}", openid, requestBody);
-            // 消息转换，将requestBody转换为bean对象（提供get，set方法）
+            // 消息转换
             MessageTextEntity message = XmlUtil.xmlToBean(requestBody, MessageTextEntity.class);
-
-            if ("event".equals(message.getMsgType()) && "SCAN".equals(message.getEvent())) {
-                loginService.saveLoginState(message.getTicket(), openid);
-                return buildMessageTextEntity(openid, "你好，这里是小张的微信测试公众号");
-            }
-
             return buildMessageTextEntity(openid, "你好，" + message.getContent());
         } catch (Exception e) {
             log.error("接收微信公众号信息请求{}失败 {}", openid, requestBody, e);
